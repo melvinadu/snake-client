d3d0d16a4638501c0a89e58262868ddc12e662d6
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-02 02:29:02 +0000

    added ability to control snake with keyboard

--- input.js
+++ input.js
@@ -1,19 +1,34 @@
-const setupInput = function (key) {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
+  
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput)
+  
+  stdin.on("data", (key) => {
+    process.stdout.write(".");
+    if (key === "\u0003") {
+      process.exit();
+    } else if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    } else if (key === "h") {
+      connection.write("Say: Hello!");
+    }
+  });
+
+
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-module.exports = {setupInput}
+module.exports = { setupInput };
 
 
--- play.js
+++ play.js
@@ -1,29 +1,10 @@
 const { connect } = require("./client")
+const { setupInput} = require("./input")
 
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
 
 // setup interface to handle user input from stdin
-
-
-
-const setupInput = function (key) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
+setupInput(connect());
 
 
